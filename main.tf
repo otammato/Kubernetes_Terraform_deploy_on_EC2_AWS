e3a60ccaba41f39ea2fb636fc0d45d9e3f3a2b35
otammato <104728608+otammato@users.noreply.github.com>
2023-03-05 16:27:08 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -142,10 +142,31 @@ resource "aws_instance" "master_instance" {
   key_name      = "test_delete"
   
   user_data     = <<-EOF
-  #!/bin/bash
-  sudo yum update -y
-  sudo amazon-linux-extras install ansible2 -y
+  sudo yum install docker -y
+  sudo systemctl start docker
+  sudo systemctl enable docker
+  sudo tee /etc/yum.repos.d/kubernetes.repo <<EOF
+  [kubernetes]
+  name=Kubernetes
+  baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+  enabled=1
+  gpgcheck=1
+  repo_gpgcheck=1
+  gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
   EOF
+  sudo yum install -y kubelet kubeadm kubectl
+
+  sudo systemctl start kubelet.service
+  sudo systemctl enable kubelet.service
+
+  sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+  kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
   
   tags = {
     Name = "master_instance"
